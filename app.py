252e55eb2760ef37eab4b914a6b9bab584501842
Zilby <azilby@gmail.com>
2014-11-10 11:38:33 -0500

    minor changes to api again

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template , request , url_for
+from flask import Flask, render_template , request , url_for, g, flash, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def mainpage():
+def home():
+  if request.method=="POST":
+    search = request.form.get("search", None)
+    data = client.tagged(search)
+    return render_template("home.html")
   return render_template("home.html")
 
 if __name__ == "__main__":
-  app.run(debug=True)
-
+  app.debug=True
+  app.run(host="127.0.0.1",port=5678)
+    
 client = pytumblr.TumblrRestClient('MSMhEiEKpmPQJtJvRUbaQlWgKoWacSP7GtmocqVZqS4UXL7TmB')
 
 #search for results using this
