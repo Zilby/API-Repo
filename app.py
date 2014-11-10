b71f6cad8fcabbe8960b4eb6fe81a286d09e5794
Zilby <azilby@gmail.com>
2014-11-10 11:13:35 -0500

    minor changes, testing needs tbd

--- app.py
+++ app.py
@@ -7,4 +7,13 @@ def mainpage():
   return render_template("home.html")
 
 if __name__ == "__main__":
-  app.run(debug=True)   
+  app.run(debug=True)
+
+client = pytumblr.TumblrRestClient(
+  'MSMhEiEKpmPQJtJvRUbaQlWgKoWacSP7GtmocqVZqS4UXL7TmB',
+  'E6cOTcJh2S6B6rzXOYgAR171Q80ST5xus10SnBTjEAKjjQ6tBz',
+  'rdayXgngXVTIJnTmlM7hN5m6r9AIgNGXgsJFgEQVXFPSaNek57',
+  'v6bxEmKLGYRS7Xicfvfz4INgeVEsi7KmHo169dVT3adCPr5PDk'
+)
+
+client.info()
