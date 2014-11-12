604a064a1e593df73012e5915f1e3b9380018bac
Alex Zilbersher <azilby@gmail.com>
2014-11-11 21:56:07 -0500

    added page for images

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
+#Must manually install pytumblr with pip install
+
 from flask import Flask, render_template , request , url_for, g, flash, redirect
 import json, pytumblr
+
+search=""
+data={}
 
 app = Flask(__name__)
 client = pytumblr.TumblrRestClient('MSMhEiEKpmPQJtJvRUbaQlWgKoWacSP7GtmocqVZqS4UXL7TmB')
 
 @app.route("/", methods=['GET','POST'])
 def home():
-  if request.method=="POST":
-    search = request.form.get("search", None)
-    data = client.tagged(search)
-    print data
+    global search,data
+    if request.method=="POST":
+        search = request.form.get("search", None)
+        data = client.tagged(search)
+        print data
+        return redirect(url_for('results'))
     return render_template("home.html")
-  return render_template("home.html")
+@app.route("/results")
+def results():
+    global search,data
+    return render_template("results.html",search=search)
 
+    
 if __name__ == "__main__":
-  app.debug=True
-  app.run(host="127.0.0.1",port=5678)
+    app.debug=True
+    app.run(host="127.0.0.1",port=5678)
 
 #search for results using this
 #client.tagged("example")
