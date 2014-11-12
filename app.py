c0ca5ba3d177f68db449abfe34fc20e7915c5a1e
Alex Zilbersher <azilby@gmail.com>
2014-11-11 23:45:57 -0500

    will now display images

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template , request , url_for, g, flash, redirect
 import json, pytumblr
 
 search=""
-data={}
+data=[]
 
 app = Flask(__name__)
 client = pytumblr.TumblrRestClient('MSMhEiEKpmPQJtJvRUbaQlWgKoWacSP7GtmocqVZqS4UXL7TmB')
@@ -14,14 +14,42 @@ def home():
     global search,data
     if request.method=="POST":
         search = request.form.get("search", None)
-        data = client.tagged(search)
-        print data
+        dic = client.tagged(search)
+        data=[]
+        for each in dic:
+            if "photos" in each:
+                data.append(each["photos"][0]['original_size']['url'])
+        for each in data:
+            print each
         return redirect(url_for('results'))
     return render_template("home.html")
+
 @app.route("/results")
 def results():
     global search,data
-    return render_template("results.html",search=search)
+    l=len(data)
+    d1=[]
+    d2=[]
+    d3=[]
+    d4=[]
+    count=-1
+    for each in data:
+        if count<(l/4):
+            d1.append(each)
+        elif count<(l/2):
+            d2.append(each)
+        elif count<(l*3/4):
+            d3.append(each)
+        else:
+            d4.append(each)
+        count+=1
+        """
+    print len(d1)
+    print len(d2)
+    print len(d3)
+    print len(d4)
+    """
+    return render_template("results.html",search=search,d1=d1,d2=d2,d3=d3,d4=d4)
 
     
 if __name__ == "__main__":
